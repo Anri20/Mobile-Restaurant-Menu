cdcc5fec545c0450c544caaa55b02ab30a4cb3cd
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-04-04 20:14:49 +0700

    Add Order Feature & Thanks Order

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { menuArray } from "./data.js"
 
 const main = document.querySelector("main div.main")
 
+// display menu
 const menu = menuArray.map(item => {
     return `
     <div id="${item.id}" class="item">
@@ -11,8 +12,8 @@ const menu = menuArray.map(item => {
             <span class="ingredients">${item.ingredients.join(", ")}</span>
             <span class="price">$${item.price}</span>
         </div>
-        <button class="add">
-            <i class="fa-solid fa-plus"></i>
+        <button class="add" data-add=${item.id}>
+            <i class="fa-solid fa-plus" data-add=${item.id}></i>
         </button>
     </div>
     `
@@ -21,3 +22,53 @@ const menu = menuArray.map(item => {
 console.log(menu)
 
 main.innerHTML = menu
+
+
+
+const orderContainer = document.querySelector("div.order")
+const orderDetail = document.querySelector("div.order-detail")
+let orderList = []
+
+// default the display to none
+orderContainer.style.display = "none"
+
+document.addEventListener("click", function (e) {
+    if (e.target.dataset.add) {
+        const food = menuArray.find(item =>
+            item.id === parseInt(e.target.dataset.add)
+        )
+        console.log(food)
+
+        orderList.push(food)
+    }
+
+    if (e.target.dataset.remove) {
+        orderList.splice(e.target.dataset.remove, 1)
+    }
+
+    const totalPrice = orderList.reduce((sum, number) => sum + number.price, 0)
+
+    let orderHTML = orderList.map((item, index) => {
+        return `
+            <p>
+                <span class="name">${item.name}</span>
+                <span class="remove" data-remove=${index}>remove</span>
+                <span class="price">$${item.price}</span>
+            </p>`
+    }).join("")
+
+    orderHTML += `
+        <hr>
+        <p>
+            <span class="name">Total Price:</span>
+            <span class="price">$${totalPrice}</span>
+        </p>`
+
+    orderDetail.innerHTML = orderHTML
+
+    if (orderList.length != 0) {
+        orderContainer.style.display = "flex"
+    } else {
+        orderContainer.style.display = "none"
+    }
+})
